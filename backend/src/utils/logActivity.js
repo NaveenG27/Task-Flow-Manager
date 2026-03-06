f5852530a383bc6a474f0112c362a0647f2fb1f3
NaveenG27 <naveeninc27@gmail.com>
2026-03-06 18:46:32 +0530

    replace supabase to prisma

--- backend/src/utils/logActivity.js
+++ backend/src/utils/logActivity.js
@@ -1,14 +1,15 @@
-import { supabase } from "../config/supabase.js";
+import { PrismaClient } from "@prisma/client";
+
+const prisma = new PrismaClient();
 
 export const logActivity = async (user_id, task_id, action) => {
   try {
-    await supabase.from("activity_logs").insert([
-      {
-        user_id,
-        task_id,
-        action
+    await prisma.log.create({
+      data: {
+        action: action,
+        taskId: task_id
       }
-    ]);
+    });
   } catch (err) {
     console.error("Activity Log Error:", err.message);
   }
